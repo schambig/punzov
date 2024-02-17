4e88613fb6829d5b729839bd2c3c9d1d666a7625
Salomon Chambi <salomonchambi@gmail.com>
2024-02-17 14:18:16 -0500

    feat: updated functions

---
+++ model2.py
@@ -0,0 +1,38 @@
+class Program:
+    def __init__(self, *expr):
+        self.instructions = expr
+
+    def __repr__(self):
+        # return "".join(f"{self.instructions}") or ""
+
+        return "\n".join([repr(expr) for expr in self.instructions])
+
+
+class BinaryOperation:
+    def __init__(self, operation,left_side, right_side):
+        self.operation = operation
+        self.left_side = left_side
+        self.right_side = right_side
+
+    def __repr__(self):
+        return f"{self.left_side} {self.operation} {self.right_side}"
+
+
+class VariableName:
+    def __init__(self, literal_name):
+        self.name = literal_name
+    
+    # Add __repr__ method to VariableName
+    def __repr__(self):
+        return self.name
+
+programa_1 = Program(
+    BinaryOperation("=", VariableName("a1"), 8),
+    BinaryOperation("=", VariableName("a2"), 9),
+    BinaryOperation(
+        "=",
+        VariableName("resultado"),
+        BinaryOperation("+", VariableName("a1"), VariableName("a2"))
+    )
+)
+print(programa_1)
