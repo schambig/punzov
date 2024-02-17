c1fc335b1f956c985fae970b922ce37257388a9f
Salomon Chambi <salomonchambi@gmail.com>
2024-02-17 14:36:47 -0500

    feat: add operations precedence

--- model2.py
+++ model2.py
@@ -17,6 +17,15 @@ class BinaryOperation:
         return f"{self.left_side} {self.operation} {self.right_side}"
 
 
+class VariableInit:
+    def __init__(self, literal_name):
+        self.name = literal_name
+    
+    # Add __repr__ method to VariableInit
+    def __repr__(self):
+        return f'var {self.name}'
+
+
 class VariableName:
     def __init__(self, literal_name):
         self.name = literal_name
@@ -25,13 +34,23 @@ class VariableName:
     def __repr__(self):
         return self.name
 
+
 programa_1 = Program(
-    BinaryOperation("=", VariableName("a1"), 8),
-    BinaryOperation("=", VariableName("a2"), 9),
+    BinaryOperation("=", VariableInit("a1"), 8),
+    BinaryOperation("=", VariableInit("a2"), 9),
     BinaryOperation(
         "=",
-        VariableName("resultado"),
-        BinaryOperation("+", VariableName("a1"), VariableName("a2"))
+        VariableInit("resultado"),
+        BinaryOperation(
+            "+",
+            BinaryOperation("*", VariableName("a1"), VariableName("a1")),
+            BinaryOperation(
+                "-",
+                BinaryOperation("*", VariableName("a2"), VariableName("a2")),
+                BinaryOperation("/", VariableName("a1"), VariableName("a2"))
+            )
+        )
     )
 )
+
 print(programa_1)
